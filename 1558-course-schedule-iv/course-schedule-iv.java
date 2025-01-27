0b02d8bbb054e8bf43ba7518edc64535a733cd8c
Pradeep Ranjan <111266003+Pradeep94GMU@users.noreply.github.com>
2025-01-26 23:02:46 -0500

    Time: 20 ms (86.62%) | Memory: 48.2 MB (51.62%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,57 +1,46 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
 
-        //pre se ek graph bana lo pahle,
-        List<List<Integer>> adj = new ArrayList();
-        int n = prerequisites.length;
-        List<Boolean> res = new ArrayList<>();
-        if(n == 0){
-            //all course are independent so we need to return all query as false
-            return new ArrayList<Boolean>(Collections.nCopies(queries.length, false));
+
+        //as constraint is less so we can think of n^3 operation aslo
+        //point to catch: queries: src -> dest ( we shlld find yes/No in minimul time to optimise this)
+        //src se dest to bahut algorihtm hai but as tc n^3 jaa skta hai to floyd warshall check kr lo
+        //usse hame O(1) find kr skte hai ki koi connected hai directly or indirectly or not
+        //shprted path nhi pucha so no need to go few algorithm, it is just asking if possible or not
+        boolean[][] isReachable = new boolean[numCourses][numCourses];
+
+        //fill the table suing intermidiate node which run like 0 -> n -1 nodes
+
+        //fill the preReq first
+        for(int[] eachPre: prerequisites){
+            int src = eachPre[0];
+            int dest = eachPre[1];
+            isReachable[src][dest] = true;
 
         }
 
-        for(int i = 0; i < numCourses; i++){
-            adj.add(new ArrayList());
-        }
 
-        for(int[] each_pre : prerequisites){
-            int u = each_pre[0];
-            int v = each_pre[1];
-            adj.get(u).add(v);
-        }
-        System.out.println(adj);
-
-        //for each queries
-        for(int[] eachQu: queries){
-            int requiredCourse = eachQu[0];
-            int targetCourse = eachQu[1];
-            boolean[] vis = new boolean[numCourses];
-            boolean isPoss = checkPreq(adj, requiredCourse, targetCourse, vis);
-            res.add(isPoss);
-        }
-
-
-
-        return res;
-        
-    }
-
-    private boolean checkPreq(List<List<Integer>> adj, int src, int dest, boolean[] vis){
-        //we can see basically we need to do src -> dest kinda thing
-        //one src and one dest= dfs se bhi kr skte hai and bfs se bhi ho jayega
-        vis[src] = true;
-
-        for(int parosi: adj.get(src)){
-            if(parosi == dest) return true;
-            if(!vis[parosi]){
-                if(checkPreq(adj, parosi, dest, vis)){
-                    return true;
+        for(int intermidiateNode = 0; intermidiateNode <  numCourses; intermidiateNode++){
+            for(int src = 0; src < numCourses; src++){
+                for(int dest = 0; dest < numCourses; dest++){
+                    //check kro ki src -> intermidate koi reachble hai and also intermidiate se dest reachlbe hai??
+                    if(isReachable[src][intermidiateNode] && isReachable[intermidiateNode][dest]){
+                        isReachable[src][dest] = true;
+                    }
                 }
             }
         }
 
-        return false;
+        List<Boolean> res = new ArrayList<>();
 
+        //now as we have our reachle data we can do quesries
+        for(int[] each: queries){
+            int src = each[0];
+            int dest = each[1];
+            res.add(isReachable[src][dest]);
+        }
+
+        return res;
+        
     }
 }
