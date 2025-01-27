90c4af55002e045d265107c498d23d47fa4fea62
Pradeep Ranjan <111266003+Pradeep94GMU@users.noreply.github.com>
2025-01-26 23:46:23 -0500

    Time: 2 ms (93.18%) | Memory: 47.1 MB (81.82%) - LeetSync

--- 531-lonely-pixel-i/lonely-pixel-i.java
+++ 531-lonely-pixel-i/lonely-pixel-i.java
@@ -3,38 +3,32 @@ class Solution {
         int m = picture.length;
         int n = picture[0].length;
         int count = 0;
-
+        int[] rC = new int[m];
+        int[] cC = new int[n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
+                //fill rC and cC
                 if(picture[i][j] == 'B'){
-                    //check kro ki iske row and col me koi black to nhi
-                    if(noBlackFoundInRC(picture, i, j)){
-                        count++;
-                    }
+                    rC[i]++;
+                    cC[j]++;
                 }
             }
         }
 
+        //now for the secord pass, we just need to check if the count == 1 for that perticular r and c
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(picture[i][j] == 'B' && rC[i] == 1 && cC[j] == 1){
+                    count++;
+                }
+            }
+        }
+
+
         return count;
+        
         
     }
 
-    private boolean noBlackFoundInRC(char[][] picture, int i, int j){ // i = 0, j = 2
-        //check in row 
-        int m = picture.length;
-        int n = picture[0].length;
-        for(int row = 0; row < m; row++){
-            //har row ke j col me check krna prega
-            if(row == i) continue;
-            if(picture[row][j] == 'B') return false;
-        }
-
-        //har col me check kaise krenge?? row fix krke
-        for(int col = 0; col < n; col++){
-            if(col == j) continue;
-            if(picture[i][col] == 'B') return false;
-        }
-
-        return true;
-    }
+    
 }
