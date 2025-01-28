30d6df3619410b9c0f10d6b9e6b0067188dde49d
Pradeep Ranjan <111266003+Pradeep94GMU@users.noreply.github.com>
2025-01-27 23:45:50 -0500

    Time: 5 ms (65.83%) | Memory: 45 MB (71.43%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -11,7 +11,7 @@ class Solution {
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(!vis[i][j] && grid[i][j] != 0){
-                    int fish = findTotalFish(grid, i, j, vis);
+                    int fish = findTotalFish(grid, i, j);
                     maxFish = Math.max(maxFish, fish);
                 }
             }
@@ -21,15 +21,16 @@ class Solution {
         
     }
 
-    private int findTotalFish(int[][] grid, int i, int j, boolean[][] vis){
-        vis[i][j] = true;
+    private int findTotalFish(int[][] grid, int i, int j){
+        
         int fish = 0;
         fish += grid[i][j];
+        grid[i][j] = 0;
         for(int[] dir: dirs){
             int nr = i + dir[0];
             int nc = j + dir[1];
-            if(nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc] && grid[nr][nc] != 0){
-                fish += findTotalFish(grid, nr, nc, vis);
+            if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] != 0){
+                fish += findTotalFish(grid, nr, nc);
             }
         }
 
